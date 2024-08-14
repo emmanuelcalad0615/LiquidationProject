e123810fdfcf4f3479483434cbb9b31a851b010f
luna1860 <saritaprinmc@gmail.com>
2024-08-14 09:31:47 -0500

    Adding final corrections

--- Liquidation.py
+++ Liquidation.py
@@ -17,10 +17,25 @@ dias faltantes del mes(con respecto al contrato) : dias_fm
 ingreso en UVT : ingreso_uvt
 porcentaje con respecto al ingreso de uvt : porcentaje_uvt
 porcentaje de retencion : porcentaje_r
-contrato a 1 año : contrato1
-contrato a menos de 1 año : contrato2
-contrato a termino indefinido : contrato3
 """
+class EmpleadoException(Exception):
+    """Clase base para excepciones relacionadas con la clase Empleado."""
+
+class ValorNegativo(Exception):
+    """Excepción para valores negativos."""
+    pass
+
+class TipoDatosIncorrecto(Exception):
+    """Excepción para tipos de datos incorrectos."""
+    pass
+
+class DivisionPorCero(Exception):
+    """Excepción para divisiones por cero."""
+    pass
+
+class NumeroFueraDeRango(Exception):
+    """Excepción para números fuera de rango."""
+    pass
 
 class Empleado:
 
@@ -37,24 +52,49 @@ class Empleado:
         self.porcentaje_r = porcentaje_r
         self.tipo_contrato = tipo_contrato
 
-    def calcular_cesantias(self,salario,prima_v,auxilio_t, dias_t):
-        cesantias = (salario + (1/12) * prima_v + auxilio_t) / (366 * dias_t)
+    def calcular_cesantias(self, salario, prima_v, auxilio_t, dias_t):
+        if dias_t <= 0:
+            raise ValorNegativo("Los días trabajados deben ser mayores que cero.")
+        try:
+            cesantias = (salario + (1/12) * prima_v + auxilio_t) / (366 * dias_t)
+        except ZeroDivisionError:
+            raise DivisionPorCero() from None
+        return cesantias
 
     def calcular_intereses(self, cesantias, dias_t):
-        interes_c = (cesantias * 0.12) / 366 * dias_t
+        if cesantias <= 0:
+            raise ValorNegativo("Las cesantías deben ser mayores que cero.")
+        try:
+            interes_c = (cesantias * 0.12) / 366 * dias_t
+        except ZeroDivisionError:
+            raise DivisionPorCero() from None
+        return interes_c
+    
+    def calcular_prima_servicios(self, salario, prima_v, auxilio_t, dias_lp):
+        if dias_lp <= 0:
+            raise ValorNegativo("Los días de licencia pagada deben ser mayores que cero.")
+        try:
+            prima_s = ((salario + (1/12 * prima_v) + auxilio_t) / 2) / (180 * dias_lp)
+        except ZeroDivisionError:
+            raise DivisionPorCero() from None
+        return prima_s
 
-    def calcular_prima_servicios(self,salario,prima_v,auxilio_t,dias_lp):
-        prima_s = ((salario + (1/12 *(prima_v)) + auxilio_t) / 2) / (180 * dias_lp)
+    def calcular_prima_vacaciones(self, salario, dias_t):
+        if dias_t <= 0:
+            raise ValorNegativo("Los días trabajados deben ser mayores que cero.")
+        try:
+            prima_v = (salario / 30) * (dias_t * 15 / 360)
+        except ZeroDivisionError:
+            raise DivisionPorCero() from None
+        return prima_v
 
-    def calcular_prima_vacaciones(self,salario,dias_t):
-        prima_v = (salario / 30) * (dias_t * 15 / 360)
-
-    def calcular_retencion(self,ingreso_uvt,porcentaje_uvt,porcentaje_r,salario):
+    def calcular_retencion(self, ingreso_uvt, porcentaje_uvt, porcentaje_r, salario):
         if self.salario > 3900000:
             ingreso_uvt = (salario / 42.412)
             retencion = (ingreso_uvt - porcentaje_uvt) * porcentaje_r
         else:
-            print(" El trabajador no debe pagar retención a la fuente")
+            print("El trabajador no debe pagar retención a la fuente")
+            return 0
 
 
     def calculos(self, salario, prima_v, auxilio_t, dias_t, dias,lp, dias_f, meses_c, dias_fm, porcentaje_uvt, porcentaje_r,dias_lp,tipo_contrato,ingreso_uvt):
