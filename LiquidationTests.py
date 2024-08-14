9fce8949ef998c3c58e3623ca3d9c99ca4a3a8d4
luna1860 <saritaprinmc@gmail.com>
2024-08-14 08:34:26 -0500

    adding new files 2

--- LiquidationTests.py
+++ LiquidationTests.py
@@ -5,6 +5,98 @@ from datetime import datetime
 class LiquidationTest(unittest.TestCase):
     # Cada método de prueba debe llamar un método assert
 
+    # Casos normales
+
+    def testLiquidation1(self):
+        salario = 5,689.500
+        prima_v = 5,741.427
+        auxilio_t = 0
+        dias_t = 729
+        dias_lp = 310
+        dias_v = 28
+        fecha_i = datetime(2019,10,28)
+        fecha_f = datetime(2020,8,8)
+        tipo_contrato = 3
+        motivo_finalizacion = 4
+        total_liquidacion = 30,741.427
+        resultado = Liquidation.Empleados.calculos(salario,auxilio_t,dias_t,dias_lp,dias_v,fecha_i,fecha_f,tipo_contrato,motivo_finalizacion)
+        self.assertEqual( total_liquidacion,resultado)
+
+    def testLiquidation2(self):
+        salario = 900.803
+        prima_v = 160.825
+        auxilio_t = 80.500
+        dias_t = 80
+        dias_lp = 59
+        dias_v = 7
+        fecha_i = datetime(2019,3,28)
+        fecha_f = datetime(2019,7,16)
+        tipo_contrato = 3
+        motivo_finalizacion = 2
+        total_liquidacion = 584.886
+        resultado = Liquidation.Empleados.calculos(salario,auxilio_t,dias_t,dias_lp,dias_v,fecha_i,fecha_f,tipo_contrato,motivo_finalizacion)
+        self.assertEqual( total_liquidacion,resultado)
+
+    def testLiquidation3(self):
+        salario = 1,450.023
+        prima_v = 340.353
+        auxilio_t = 120.018
+        dias_t = 169
+        dias_lp = 30
+        dias_v = 20
+        fecha_i = datetime(2018,1,14)
+        fecha_f = datetime(2018,8,8)
+        tipo_contrato = 2
+        motivo_finalizacion = 4
+        total_liquidacion = 1,590.109
+        resultado = Liquidation.Empleados.calculos(salario,auxilio_t,dias_t,dias_lp,dias_v,fecha_i,fecha_f,tipo_contrato,motivo_finalizacion)
+        self.assertEqual( total_liquidacion,resultado)
+
+    def testLiquidation4(self):
+        salario = 3,638.092
+        prima_v = 4,486.980
+        auxilio_t = 0
+        dias_t = 888
+        dias_lp = 154
+        dias_v = 16
+        fecha_i = datetime(2021,12,17)
+        fecha_f = datetime(2024,6,4)
+        tipo_contrato = 1
+        motivo_finalizacion = 2
+        total_liquidacion = 22,160.508
+        resultado = Liquidation.Empleados.calculos(salario,auxilio_t,dias_t,dias_lp,dias_v,fecha_i,fecha_f,tipo_contrato,motivo_finalizacion)
+        self.assertEqual( total_liquidacion,resultado)
+    
+    def testLiquidation5(self):
+        salario = 5,128.350
+        prima_v = 854.725
+        auxilio_t = 500.854
+        dias_t = 120
+        dias_lp = 79
+        dias_v = 15
+        fecha_i = datetime(2015,1,15)
+        fecha_f = datetime(2015,7,28)
+        tipo_contrato = 1
+        motivo_finalizacion = 1
+        total_liquidacion = 4,896.205
+        resultado = Liquidation.Empleados.calculos(salario,auxilio_t,dias_t,dias_lp,dias_v,fecha_i,fecha_f,tipo_contrato,motivo_finalizacion)
+        self.assertEqual( total_liquidacion,resultado)
+
+    def testLiquidation6(self):
+        salario = 877.803
+        prima_v = 256.026
+        auxilio_t = 102.854
+        dias_t = 210
+        dias_lp = 169
+        dias_v = 21
+        fecha_i = datetime(2011,1,14)
+        fecha_f = datetime(2011,12,7)
+        tipo_contrato = 3
+        motivo_finalizacion = 2
+        total_liquidacion = 1,584.509
+        resultado = Liquidation.Empleados.calculos(salario,auxilio_t,dias_t,dias_lp,dias_v,fecha_i,fecha_f,tipo_contrato,motivo_finalizacion)
+        self.assertEqual( total_liquidacion,resultado)
+
     # Casos excepcionales
     def testLiquidation7(self):   
         salario = 5,689.500
