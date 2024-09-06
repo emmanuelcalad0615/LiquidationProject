a0f08a6c5c6efd9846555bd34e3cf5be55650ec6
luna1860 <saritaprinmc@gmail.com>
2024-09-05 23:37:57 -0500

    changing employee

--- src/Logic/employee.py
+++ src/Logic/employee.py
@@ -9,11 +9,11 @@ class Employee:
         self.one_twelfth_vacation_bonus = one_twelfth_vacation_bonus
         self.worked_days = worked_days 
         self.severance_pay_for_accrued_leave_days = severance_pay_for_accrued_leave_days 
-        self.PERCENTAGE_OF_SEVERANCE_PAY = 0.12 # porcentaje de las cesantias
+        self.PERCENTAGE_OF_SEVERANCE_PAY = 0.12 
         self.MONTHS_OF_THE_YEAR = 12
         self.DAYS_OF_THE_YEAR = 360 
         self.VACATION_PER_YEAR = 15 
         self.DAYS_PER_MONTH = 30
         self.HALF_A_SEMESTER_WORKED = 2 
         self.DAYS_WORKED_IN_THE_SEMESTER = 180 
-        self.TOTAL_SEMIANNUAL_PREMIUM_INSTALLMENTS = 0.6 # Cuotas Totales de Prima Semestral
+        self.TOTAL_SEMIANNUAL_PREMIUM_INSTALLMENTS = 0.6 
