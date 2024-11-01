0dacc6cae59ee029668059155597fe336123ec90
emmanuelcalad0615 <emmanuelcalad@gmail.com>
2024-11-01 15:20:52 -0500

    Configurate secret config

---
+++ src/Controller/ConfigSample.py
@@ -0,0 +1,11 @@
+"""
+Fill in this information and save this file as Secret_Config.py
+to be able to run the application.
+
+"""
+
+PGDATABASE = "ENTER THE NAME OF THE DATABASE"
+PGUSER = "ENTER THE DATABASE USER"
+PGPASSWORD = "ENTER THE PASSWORD"
+PGHOST = "ENTER THE DNS ADDRESS OR IP ADDRESS OF THE SERVER"
+PGPORT = 5432 # DEFAULT IS 5432, BUT IT MAY CHANGE IN YOUR DB
--- src/Controller/SecretConfig.py
+++
@@ -1,5 +0,0 @@
-PGHOST='ep-small-sun-a56bpuzl.us-east-2.aws.neon.tech'
-PGDATABASE='liquidation'
-PGUSER='liquidation_owner'
-PGPASSWORD='Ndx1cJ5nlBFT'
-PGPORT='5432'
